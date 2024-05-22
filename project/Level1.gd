16b5b2e2d5417b3f1fc5f000de907a144a210eba
Texan Doom Guy <ooofvill@gmail.com>
2024-05-22 07:39:58 -0500

    Level 1
    yippe

--- project/Level1.gd
+++ project/Level1.gd
@@ -1,7 +1,9 @@
 extends Node2D
+@onready var player = $Player
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Player.restart(Vector2(258,-87))
+	player.restart(Vector2(258,-87))
+	player.spawn_point = Vector2(258,-87)
 	
